73529b59d340ec14ea2b7c6648c2f66b30e05f19
Eren Kazan <erennkazan@gmail.com>
2023-03-09 00:39:12 +0300

    gulpfile fixes

--- gulpfile.js
+++ gulpfile.js
@@ -7,17 +7,6 @@ const plumber = require('gulp-plumber')
 const pug = require('gulp-pug')
 const reload = browserSync.reload
 
-function buildStyles() {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-};
-
-exports.buildStyles = buildStyles;
-exports.watch = function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
-};
-
 gulp.task('browser-sync', function () {
   browserSync.init({
     notify: false,
@@ -25,24 +14,23 @@ gulp.task('browser-sync', function () {
       baseDir: './'
     }
   })
-  gulp.watch('./*.html').on('change', reload) /*html değiştiğinde reload yapar */
-  gulp.watch('./scss/*.scss').on('change', reload) /*css değiştiğinde */
-  gulp.watch('./views/**/*.pug', gulp.series('html'))
-  gulp.watch('./scss/**/*.scss', gulp.series('css'))
-  gulp.watch('./js/**/*.js', reload)
+  gulp.watch('*.html').on('change', reload) /*html değiştiğinde reload yapar */
+  gulp.watch('sass/*.scss').on('change', reload); /*css değiştiğinde */
+  gulp.watch('views/**/*.pug', gulp.series('html'))
+  gulp.watch('sass/**/*.scss', gulp.series('css'))
+  gulp.watch('js/**/*.js', reload)
 })
 
 gulp.task('css', () => {
-  return gulp.src('./scss/main.scss')
+  return gulp.src('./sass/main.scss')
   .pipe(plumber([{ errorHandler: false }]))
   .pipe(sass())
   .pipe(prefix())
   .pipe(gulp.dest('./'))
-  .pipe(browserSync.stream())
+  .on('end', reload)
 })
 
-
-gulp.task('html', ()=> {
+gulp.task('html', () => {
   return gulp.src('views/*.pug')
   .pipe(pug())
   .pipe(gulp.dest('./'))
